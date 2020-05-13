dd72a45cadfafe495b8644af61582ae22b0c7ad0
ivanbakrac <48951541+ivanbakrac@users.noreply.github.com>
2020-05-13 07:24:14 -0400

    Update SK_Regression.py

--- SK_Regression.py
+++ SK_Regression.py
@@ -6,23 +6,37 @@ from sklearn.datasets import load_boston
 
 boston = load_boston()
 boston_df = boston.data
-boston_df = pd.DataFrame(boston_df,columns=boston.feature_names)
+boston_df = pd.DataFrame(boston_df, columns=boston.feature_names)
 target = pd.DataFrame(boston.target, columns=["MEDV"])
 
-X = boston_df[['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 
-               'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']]
-Y = target['MEDV']
+X = boston_df[
+    [
+        "CRIM",
+        "ZN",
+        "INDUS",
+        "CHAS",
+        "NOX",
+        "RM",
+        "AGE",
+        "DIS",
+        "RAD",
+        "TAX",
+        "PTRATIO",
+        "B",
+        "LSTAT",
+    ]
+]
+Y = target["MEDV"]
 
 lm = LinearRegression()
-lm.fit(X,Y)
-coeff_df = pd.DataFrame(lm.coef_,X.columns,columns=['Coefficient'])
+lm.fit(X, Y)
+coeff_df = pd.DataFrame(lm.coef_, X.columns, columns=["Coefficient"])
 
-coeff_df=abs(coeff_df)
-coeff_df=coeff_df.sort_values(by=['Coefficient'])
+coeff_df = abs(coeff_df)
+coeff_df = coeff_df.sort_values(by=["Coefficient"])
 
 print(coeff_df)
 print(coeff_df[-1:])
-
 
 
 
