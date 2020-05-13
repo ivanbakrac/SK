e6dc071b81ca8f930cb4c994879fff2e49353eda
ivanbakrac <48951541+ivanbakrac@users.noreply.github.com>
2020-05-13 07:26:48 -0400

    Update SK_KMeans_3.py

--- SK_KMeans_3.py
+++ SK_KMeans_3.py
@@ -5,17 +5,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 iris = datasets.load_iris()
-x = pd.DataFrame(iris.data, columns=['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width'])
-y = pd.DataFrame(iris.target, columns=['Target'])
+x = pd.DataFrame(
+    iris.data, columns=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width"]
+)
+y = pd.DataFrame(iris.target, columns=["Target"])
 
-plt.figure(figsize=(12,5))
-colors = np.array(['red', 'green', 'blue'])
+plt.figure(figsize=(12, 5))
+colors = np.array(["red", "green", "blue"])
 plt.subplot(1, 2, 1)
-plt.scatter(x['Sepal Length'], x['Sepal Width'], c=colors[y['Target']], s=40)
-plt.title('Sepal Length vs Sepal Width')
-plt.subplot(1,2,2)
-plt.scatter(x['Petal Length'], x['Petal Width'], c=colors[y['Target']], s=40)
-plt.title('Petal Length vs Petal Width')
+plt.scatter(x["Sepal Length"], x["Sepal Width"], c=colors[y["Target"]], s=40)
+plt.title("Sepal Length vs Sepal Width")
+plt.subplot(1, 2, 2)
+plt.scatter(x["Petal Length"], x["Petal Width"], c=colors[y["Target"]], s=40)
+plt.title("Petal Length vs Petal Width")
 
 # Step1. Search for optimal K
 
@@ -23,7 +25,7 @@ SSE = {}
 for i in range(1, 10):
     kmeans = KMeans(n_clusters=i, random_state=0).fit(x)
     SSE[i] = kmeans.inertia_
-print (SSE)
+print(SSE)
 
 
 fig = plt.figure()
@@ -31,8 +33,18 @@ ax = fig.add_subplot(111)
 plt.plot(list(SSE.keys()), list(SSE.values()))
 plt.xlabel("Number of cluster")
 plt.ylabel("SSE")
-ax.annotate('elbow', xy=(2, 150), xytext=(3, 170), arrowprops=dict(facecolor='black', shrink=0.05))
-ax.annotate('elbow', xy=(3, 90), xytext=(3, 170), arrowprops=dict(facecolor='black', shrink=0.05))
+ax.annotate(
+    "elbow",
+    xy=(2, 150),
+    xytext=(3, 170),
+    arrowprops=dict(facecolor="black", shrink=0.05),
+)
+ax.annotate(
+    "elbow",
+    xy=(3, 90),
+    xytext=(3, 170),
+    arrowprops=dict(facecolor="black", shrink=0.05),
+)
 plt.show()
 
 # Step2 Fit with K=3 given SSE "optimal"
@@ -42,21 +54,18 @@ model.fit(x)
 
 print(model.labels_)
 print(iris.target)
-colors = np.array(['red', 'green', 'blue'])
+colors = np.array(["red", "green", "blue"])
 
-# Step3: Compare Actual vs Predicted 
+# Step3: Compare Actual vs Predicted
 
-plt.figure(figsize=(12,5))
+plt.figure(figsize=(12, 5))
 predictedY = np.choose(model.labels_, [1, 0, 2]).astype(np.int64)
 
 plt.subplot(1, 2, 1)
-plt.scatter(x['Petal Length'], x['Petal Width'], c=colors[y['Target']], s=40)
-plt.title('Before classification')
+plt.scatter(x["Petal Length"], x["Petal Width"], c=colors[y["Target"]], s=40)
+plt.title("Before classification")
 
 plt.subplot(1, 2, 2)
-plt.scatter(x['Petal Length'], x['Petal Width'], c=colors[predictedY], s=40)
+plt.scatter(x["Petal Length"], x["Petal Width"], c=colors[predictedY], s=40)
 plt.title("Model's classification")
-
-
-
 
